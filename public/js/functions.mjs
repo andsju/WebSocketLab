7f6735f3e6e0d54eb3157cf14085b351f6cf4be8
unknown <anders.sjunnesson@gmail.com>
2021-09-29 13:18:42 +0200

    Document functions

--- public/js/functions.mjs
+++ public/js/functions.mjs
@@ -1,3 +1,10 @@
+/**
+ * parse
+ *
+ * @export
+ * @param {*} data
+ * @return {*} 
+ */
 export function parse(data) {
     try {
         return JSON.parse(data);
@@ -8,30 +15,44 @@ export function parse(data) {
 }
 
 // function to add new messages
+/**
+ * add chat message to DOM
+ *
+ * @export
+ * @param {*} user
+ * @param {*} message
+ * @param {*} date
+ * @return {*} 
+ */
 export function addMessage(user, message, date) {
 
-    // validate arguments...
+    // to do: validate arguments...
+
     // template clone
     let tpl = document.querySelector("#message").cloneNode(true);
-
-    // console.log("tpl", tpl);
-
     tpl = tpl.content;
     tpl.querySelector("span").textContent = user;
     tpl.querySelector("p").textContent = message;
     tpl.querySelector("time").textContent = formatDate(date);
     tpl.querySelector("time").setAttribute("datetime", date.toISOString());
+
     return document.body.querySelector("#conversation").append(tpl);
 }
 
-// function to return date in HH:MM:ss
+/**
+ * get datetime in HH:MM:ss
+ *
+ * @export
+ * @param {*} date
+ * @return {*} 
+ */
 export function formatDate(date) {
 
     if (date instanceof Date !== true) {
         return "";
     }
 
-    // hours...
+    // pad leading 0
     let hours = date.getHours();
     hours = hours < 10 ? "0" + hours : hours;
 
