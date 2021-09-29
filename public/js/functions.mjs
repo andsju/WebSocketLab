17f19730927a116df3cb20e08087f70127e47b9b
unknown <anders.sjunnesson@gmail.com>
2021-09-29 10:18:39 +0200

    Functions

--- public/js/functions.mjs
+++ public/js/functions.mjs
@@ -6,3 +6,41 @@ export function parse(data) {
         return;
     }
 }
+
+// function to add new messages
+export function addMessage(user, message, date) {
+
+    // validate arguments...
+    // template clone
+    let tpl = document.querySelector("#message").cloneNode(true);
+
+    // console.log("tpl", tpl);
+
+    tpl = tpl.content;
+    tpl.querySelector("span").textContent = user;
+    tpl.querySelector("p").textContent = message;
+    tpl.querySelector("time").textContent = formatDate(date);
+    tpl.querySelector("time").setAttribute("datetime", date.toISOString());
+    return document.body.querySelector("#conversation").append(tpl);
+}
+
+// function to return date in HH:MM:ss
+export function formatDate(date) {
+
+    if (date instanceof Date !== true) {
+        return "";
+    }
+
+    // hours...
+    let hours = date.getHours();
+    hours = hours < 10 ? "0" + hours : hours;
+
+    let minutes = date.getMinutes();
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    let seconds = date.getSeconds();
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    // HH:MM:ss
+    return hours + ":" + minutes + ":" + seconds;
+}
